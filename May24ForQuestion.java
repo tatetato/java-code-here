7a2fb433eecd20c14b72219d7238b4c08584496d
tatetato <167390540+tatetato@users.noreply.github.com>
2024-05-24 11:22:18 +0900

    Update May24ForQuestion.java

--- May24ForQuestion.java
+++ May24ForQuestion.java
@@ -1,5 +1,17 @@
 public class May24ForQuestion {
     public static void main(String[] args) {
+
+        int rest = 100 % 13;
+
+        for (int h = 100 + (13 - rest); h < 200; h = h + 13) {
+            System.out.print(h + " ");
+        }
+
+        System.out.println();
+        System.out.println("-------");
+
+        System.out.println(rest);
+
         for (int i = 100; i < 200; i++) {
             if (i % 13 == 0) {
                 System.out.print(i + " ");
