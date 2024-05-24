901a38174e3a36f2cb8861d580e90251f035fee1
tatetato <167390540+tatetato@users.noreply.github.com>
2024-05-24 12:23:47 +0900

    Update May24Calendar.java

--- May24Calendar.java
+++ May24Calendar.java
@@ -11,8 +11,16 @@ public class May24Calendar {
 
         String[] days1 = { "日", "月", "火", "水", "木", "金", "土" };
         int[] m = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-        String today1String = "三月四日";
-        int today1 = 0 + 31 + 29 + 4;
+
+        // 三月四日
+        int month = 3;
+        int date = 4;
+
+        int today1 = 0;
+        for (int a = 0; a < month; a++) {
+            today1 = today1 + m[a];
+        }
+        today1 = today1 + date;
         System.out.println(days1[today1 % 7] + "曜日");
     }
 }
