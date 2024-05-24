61324938b8a63cd923506979cfbe0891c1ce59d1
tatetato <167390540+tatetato@users.noreply.github.com>
2024-05-24 11:55:02 +0900

    Update May24ForQuestion.java

--- May24ForQuestion.java
+++ May24ForQuestion.java
@@ -1,6 +1,18 @@
 public class May24ForQuestion {
     public static void main(String[] args) {
 
+        int rest1 = 50 % 9;
+
+        for (int h = 50 + (9 - rest1) + 4; h < 100; h = h + 9) {
+            System.out.print(h + ", ");
+            System.out.print(h - 4 + " + 4, ");
+            System.out.print((h - 4) / 9 + " * 9 + 4, ");
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("-------");
+
         int rest = 100 % 13;
 
         for (int h = 100 + (13 - rest); h < 200; h = h + 13) {
